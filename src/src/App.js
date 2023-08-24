0018b9a26c1b53281397fbc075aa16239ae42b22
dilesoft <dilesoft@gmail.com>
2023-08-24 19:16:54 +0300

    iobroker matter

---
+++ src/src/App.js
@@ -0,0 +1,114 @@
+import React from 'react';
+import { withStyles } from '@mui/styles';
+import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles';
+
+import AppBar from '@mui/material/AppBar';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Button from '@mui/material/Button';
+
+import GenericApp from '@iobroker/adapter-react-v5/GenericApp';
+import { I18n, Loader, AdminConnection } from '@iobroker/adapter-react-v5';
+
+const styles = theme => ({
+    root: {},
+    tabContent: {
+        padding: 10,
+        height: 'calc(100% - 64px - 48px - 20px)',
+        overflow: 'auto',
+    },
+    tabContentIFrame: {
+        padding: 10,
+        height: 'calc(100% - 64px - 48px - 20px - 38px)',
+        overflow: 'auto',
+    },
+    selected: {
+        color: theme.palette.mode === 'dark' ? undefined : '#FFF !important',
+    },
+    indicator: {
+        backgroundColor: theme.palette.mode === 'dark' ? theme.palette.secondary.main : '#FFF',
+    },
+});
+
+class App extends GenericApp {
+    constructor(props) {
+        const extendedProps = { ...props };
+        extendedProps.encryptedFields = ['pass'];
+        extendedProps.Connection = AdminConnection;
+        extendedProps.translations = {
+            en: require('./i18n/en'),
+            de: require('./i18n/de'),
+            ru: require('./i18n/ru'),
+            pt: require('./i18n/pt'),
+            nl: require('./i18n/nl'),
+            fr: require('./i18n/fr'),
+            it: require('./i18n/it'),
+            es: require('./i18n/es'),
+            pl: require('./i18n/pl'),
+            uk: require('./i18n/uk'),
+            'zh-cn': require('./i18n/zh-cn'),
+        };
+
+        extendedProps.sentryDSN = window.sentryDSN;
+        // extendedProps.socket = {
+        //     protocol: 'http:',
+        //     host: '192.168.178.45',
+        //     port: 8081,
+        // };
+
+        super(props, extendedProps);
+
+        this.state.selectedTab = window.localStorage.getItem(`${this.adapterName}.${this.instance}.selectedTab`) || 'options';
+    }
+
+    onConnectionReady() {
+        this.socket.getState(`${this.adapterName}.${this.instance}.info.ackTempPassword`)
+            .then(state => {
+                if (!state || !state.val) {
+                    this.setState({ showAckTempPasswordDialog: true });
+                }
+            });
+    }
+    render() {
+        if (!this.state.loaded) {
+            return <StyledEngineProvider injectFirst>
+                <ThemeProvider theme={this.state.theme}>
+                    <Loader theme={this.state.themeType} />
+                </ThemeProvider>
+            </StyledEngineProvider>;
+        }
+
+        return <StyledEngineProvider injectFirst>
+            <ThemeProvider theme={this.state.theme}>
+                <div className="App" style={{ background: this.state.theme.palette.background.default, color: this.state.theme.palette.text.primary }}>
+                    <AppBar position="static">
+                        <Tabs
+                            value={this.state.selectedTab || 'options'}
+                            onChange={(e, value) => {
+                                this.setState({ selectedTab: value });
+                                window.localStorage.setItem(`${this.adapterName}.${this.instance}.selectedTab`, value);
+                            }}
+                            scrollButtons="auto"
+                            classes={{ indicator: this.props.classes.indicator }}
+                        >
+                            <Tab />
+                        </Tabs>
+                    </AppBar>
+
+                    <div className={this.isIFrame ? this.props.classes.tabContentIFrame : this.props.classes.tabContent}>
+                       
+                    </div>
+                    {this.renderError()}
+                    {this.renderSaveCloseButtons()}
+                </div>
+            </ThemeProvider>
+        </StyledEngineProvider>;
+    }
+}
+
+export default withStyles(styles)(App);

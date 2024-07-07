77928eefda62c75c3b44adfd181fdccb227682db
Ingo Fischer <github@fischer-ka.de>
2024-07-07 11:20:34 +0200

    update matter lib to 0.10.x and Add AsyncStorage (#26)
    * update matter lib to 0.10.x
    
    This also includes upgrading all clusters and such to Matter 1.3
    
    * latest update lib and Async Storage
    
    * latest update lib and Async Storage

--- src/matter/ControllerNode.ts
+++ src/matter/ControllerNode.ts
@@ -231,7 +231,7 @@ class Controller {
             },
         });
 
-        await this.adapter.setState('controller.info.discovering', false);
+        await this.adapter.setState('controller.info.discovering', false, true);
 
         await this.commissioningController.start();
 
@@ -689,9 +689,10 @@ class Controller {
             passcode = pairingCodeCodec.passcode;
         } else if (qrCode) {
             const pairingCodeCodec = QrPairingCodeCodec.decode(qrCode);
-            longDiscriminator = pairingCodeCodec.discriminator;
+            // TODO handle the case where multiple devices are included
+            longDiscriminator = pairingCodeCodec[0].discriminator;
             shortDiscriminator = undefined;
-            passcode = pairingCodeCodec.passcode;
+            passcode = pairingCodeCodec[0].passcode;
         }
         if (device) {
             longDiscriminator = undefined;
@@ -713,13 +714,18 @@ class Controller {
 
         // this.adapter.log.debug(`Commissioning ... ${JSON.stringify(options)}`);
         try {
-            const nodeObject = await this.commissioningController.commissionNode(options);
-            this.matterNodeIds.push(nodeObject.nodeId);
+            const nodeId = await this.commissioningController.commissionNode(options);
+            this.matterNodeIds.push(nodeId);
+            const nodeObject = this.commissioningController.getConnectedNode(nodeId);
+            if (nodeObject === undefined) {
+                // should never happen
+                throw new Error(`Node ${nodeId} is not connected but commissioning was successful. Should never happen.`);
+            }
 
             await this.nodeToIoBrokerStructure(nodeObject);
 
-            this.adapter.log.debug(`Commissioning successfully done with nodeId ${nodeObject.nodeId}`);
-            return { result: true, nodeId: nodeObject.nodeId.toString() };
+            this.adapter.log.debug(`Commissioning successfully done with nodeId ${nodeId}`);
+            return { result: true, nodeId: nodeId.toString() };
         } catch (error) {
             this.adapter.log.debug(`Commissioning failed: ${error}`);
             return { error, result: false };
@@ -730,7 +736,7 @@ class Controller {
         if (!this.commissioningController) {
             return null;
         }
-        await this.adapter.setState('controller.info.discovering', { val: true, ack: true } );
+        await this.adapter.setState('controller.info.discovering', true, true );
         this.discovering = true;
         this.adapter.log.info(`Start the discovering...`);
         const result = await this.commissioningController.discoverCommissionableDevices(
@@ -761,7 +767,7 @@ class Controller {
         if (this.commissioningController && this.discovering) {
             this.adapter.log.info(`Stop the discovering...`);
             this.discovering = false;
-            await this.adapter.setState('controller.info.discovering', { val: false, ack: true });
+            await this.adapter.setState('controller.info.discovering', false, true);
             this.commissioningController.cancelCommissionableDeviceDiscovery(
                 {},
                 {

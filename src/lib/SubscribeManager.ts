93535b57b7c40ca8a3069cccdb5a0fd95fc7116e
Ingo Fischer <github@fischer-ka.de>
2024-07-09 13:22:18 +0200

    Refactor SubscribeManager
    Adapter own states will be subscribed when needed via a wildcard subscribe and unsubscribed when needed. rest "external" states are subscribed one by one

--- src/lib/SubscribeManager.ts
+++ src/lib/SubscribeManager.ts
@@ -1,36 +1,59 @@
 type SubscribeCallback = (state: ioBroker.State) => void;
 
 class SubscribeManager {
-    static subscribes: { [id: string]: SubscribeCallback[] } = {};
+    static subscribes = new Map<string, SubscribeCallback[]>();
+    static locallySubscribed = false;
     static adapter: ioBroker.Adapter;
     static setAdapter(adapter: ioBroker.Adapter): void {
         SubscribeManager.adapter = adapter;
     }
     static observer(id: string, state: ioBroker.State | null | undefined): void {
-        if (state && SubscribeManager.subscribes[id]) {
-            SubscribeManager.subscribes[id].forEach(callback => callback(state));
+        const callbacks = SubscribeManager.subscribes.get(id);
+        if (callbacks !== undefined && state && !state.ack) {
+            callbacks.forEach(callback => callback(state));
         }
     }
 
     static async subscribe(id: string, callback: SubscribeCallback): Promise<void> {
-        if (!SubscribeManager.subscribes[id]) {
-            SubscribeManager.subscribes[id] = [];
-            // SubscribeManager.adapter.log.debug(`Subscribe to "${id}"`);
-            await SubscribeManager.adapter.subscribeForeignStatesAsync(id);
+        const localSubscribe = id.startsWith(SubscribeManager.adapter.namespace);
+        if (localSubscribe && !SubscribeManager.locallySubscribed) {
+            SubscribeManager.adapter.subscribeStates('*');
+            SubscribeManager.locallySubscribed = true;
+        }
+        let subscribes = SubscribeManager.subscribes.get(id);
+        if (subscribes === undefined) {
+            subscribes = [];
+            if (!localSubscribe) {
+                // SubscribeManager.adapter.log.debug(`Subscribe to "${id}"`);
+                await SubscribeManager.adapter.subscribeForeignStatesAsync(id);
+            }
         }
 
-        SubscribeManager.subscribes[id].push(callback);
+        subscribes.push(callback);
+        SubscribeManager.subscribes.set(id, subscribes);
     }
 
     static async unsubscribe(id: string, callback: SubscribeCallback): Promise<void> {
-        const pos = SubscribeManager.subscribes[id].indexOf(callback);
+        const subscribes = SubscribeManager.subscribes.get(id);
+        if (subscribes === undefined) return;
+        const pos = subscribes.indexOf(callback);
         if (pos !== -1) {
-            SubscribeManager.subscribes[id].splice(pos, 1);
+            subscribes.splice(pos, 1);
         }
-        if (!SubscribeManager.subscribes[id].length) {
+        if (!subscribes.length) {
             // SubscribeManager.adapter.log.debug(`Unsubscribe from "${id}"`);
-            delete SubscribeManager.subscribes[id];
-            await SubscribeManager.adapter.unsubscribeForeignStatesAsync(id);
+            SubscribeManager.subscribes.delete(id);
+            const namespace = SubscribeManager.adapter.namespace;
+            if (id.startsWith(namespace)) {
+                if (Array.from(SubscribeManager.subscribes.keys()).every(id => !id.startsWith(namespace))) {
+                    SubscribeManager.adapter.unsubscribeStates('*');
+                    SubscribeManager.locallySubscribed = false;
+                }
+            } else {
+                await SubscribeManager.adapter.unsubscribeForeignStatesAsync(id);
+            }
+        } else {
+            SubscribeManager.subscribes.set(id, subscribes);
         }
     }
 }

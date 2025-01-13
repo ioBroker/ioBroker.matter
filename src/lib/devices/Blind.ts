ba19af95b4adb0d089be914ec1c0f3b701c21026
Ingo Fischer <github@fischer-ka.de>
2025-01-13 17:31:45 +0100

    Fix

--- src/lib/devices/Blind.ts
+++ src/lib/devices/Blind.ts
@@ -59,6 +59,13 @@ class Blind extends BlindButtons {
         return this.#getLevelState.value;
     }
 
+    async setLevelActual(value: number): Promise<void> {
+        if (!this.#setLevelState) {
+            throw new Error('Level state not found');
+        }
+        await this.#setLevelState.setValue(value);
+    }
+
     async updateLevelActual(value: number): Promise<void> {
         if (!this.#getLevelState) {
             throw new Error('Level state not found');

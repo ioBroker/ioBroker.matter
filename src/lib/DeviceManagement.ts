7cdc57875913c264228d42a250a0125e96a44cff
Ingo Fischer <github@fischer-ka.de>
2025-01-25 14:20:58 +0100

    Revert "incorporate #347"
    This reverts commit bd519a5d35e473fbdf0b0f223490ffd100c8137a.

--- src/lib/DeviceManagement.ts
+++ src/lib/DeviceManagement.ts
@@ -123,7 +123,7 @@ class MatterAdapterDeviceManagement extends DeviceManagement<MatterAdapter> {
     /**
      * Create the "Node" device entry and also add all Endpoint-"Devices" for Device-Manager
      */
-    async #getNodeEntry(ioNode: GeneralMatterNode, backgroundColor: 'primary' | 'secondary'): Promise<DeviceInfo[]> {
+    async #getNodeEntry(ioNode: GeneralMatterNode, color: 'primary' | 'secondary'): Promise<DeviceInfo[]> {
         const status: DeviceStatus = await ioNode.getStatus();
         const isEnabled = ioNode.isEnabled;
         const isConnected = ioNode.isConnected;
@@ -193,8 +193,8 @@ class MatterAdapterDeviceManagement extends DeviceManagement<MatterAdapter> {
             connectionType,
             hasDetails: true,
             actions: actions.length ? (actions as DeviceAction<'adapter'>[]) : undefined,
-            backgroundColor,
-            color: '#002346',
+            color,
+            headerTextColor: '#002346',
             group: {
                 key: 'node',
                 name: this.#adapter.getText('Node'),
@@ -213,7 +213,7 @@ class MatterAdapterDeviceManagement extends DeviceManagement<MatterAdapter> {
                     details,
                     isConnected,
                     connectionType,
-                    backgroundColor,
+                    color,
                 );
                 res.push(deviceInfo);
                 deviceCount++;
@@ -236,7 +236,7 @@ class MatterAdapterDeviceManagement extends DeviceManagement<MatterAdapter> {
         nodeDetails: NodeDetails,
         nodeConnected: boolean,
         nodeConnectionType: ConfigConnectionType,
-        backgroundColor: 'primary' | 'secondary',
+        color: 'primary' | 'secondary',
     ): Promise<DeviceInfo> {
         const icon = device.iconDeviceType;
         const data: DeviceInfo = {
@@ -244,7 +244,7 @@ class MatterAdapterDeviceManagement extends DeviceManagement<MatterAdapter> {
             name: device.name,
             icon,
             ...nodeDetails,
-            backgroundColor,
+            color,
             status: await device.getStatus({
                 connection: nodeConnected ? 'connected' : 'disconnected',
             }),
